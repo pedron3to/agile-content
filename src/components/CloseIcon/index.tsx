28b8f7e0366bdf4de6ec409cda8ead3716567a46
Pedro Neto <pedro_neto@outlook.com>
2023-09-05 02:50:22 -0300

    Create CloseIcon

---
+++ src/components/CloseIcon/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CloseIcon = () => (
+  <svg
+    className="h-6 w-6 fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M18 6L6 18M6 6l12 12"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CloseIcon;
