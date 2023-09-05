3eb0350e1542b1f2e0c226b183ed3c053aed9b12
Pedro Neto <pedro_neto@outlook.com>
2023-09-05 14:54:22 -0300

    Add memo to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, memo } from "react";
 import { Footer, Header } from ".";
 
 interface LayoutProps {
@@ -7,22 +7,22 @@ interface LayoutProps {
   children: ReactNode;
   isHomePage?: boolean;
 }
-const Layout = ({
-  isSelected = false,
-  children,
-  isHomePage = false,
-}: LayoutProps) => {
-  return (
-    <main className="h-[100vh]  flex flex-col">
-      <div className="flex-grow">
-        <Header isSelected={isSelected} isHomePage={isHomePage} />
-        <div>{children}</div>
-      </div>
-      <div className="mt-auto">
-        <Footer isSelected={isSelected} />
-      </div>
-    </main>
-  );
-};
+const Layout = memo(
+  ({ isSelected = false, children, isHomePage = false }: LayoutProps) => {
+    return (
+      <main className="h-[100vh]  flex flex-col">
+        <div className="flex-grow">
+          <Header isSelected={isSelected} isHomePage={isHomePage} />
+          <div>{children}</div>
+        </div>
+        <div className="mt-auto">
+          <Footer isSelected={isSelected} />
+        </div>
+      </main>
+    );
+  }
+);
+
+Layout.displayName = "Layout";
 
 export default Layout;
