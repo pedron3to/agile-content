ec2228bd1f24e878b7865c2dd49d668253ddb824
Pedro Neto <pedro_neto@outlook.com>
2023-09-05 14:53:02 -0300

    Refactor close icon

--- src/components/CloseIcon/index.tsx
+++ src/components/CloseIcon/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-const CloseIcon = () => (
+interface CloseIconProps {
+  color?: string;
+}
+
+const CloseIcon = ({ color = "white" }: CloseIconProps) => (
   <svg
-    className="h-6 w-6 fill-current"
+    className="h-5 w-5"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 20 20"
+    fill={color}
   >
     <path
       d="M18 6L6 18M6 6l12 12"
